d70cc1aaf59e19a959a64d785071c40c6456ce6a
Nico Felbinger <nico@felbinger.eu>
2024-12-02 20:18:06 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Flake for Gradient";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    gradient.url = "github:Wavelens/Gradient";
+    search = {
+      url = "github:NuschtOS/search";
+      inputs = {
+        nixpkgs.follows = "nixpkgs-unstable";
+      };
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs-unstable,
+      gradient,
+      search,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = (import nixpkgs-unstable) {
+            inherit system;
+          };
+        in
+        {
+          packages = {
+            default = search.packages.${system}.mkSearch {
+              specialArgs.modulesPath = nixpkgs-unstable + "/nixos/modules";
+              modules = [
+                gradient.nixosModules.default
+                {
+                  _module.args = { inherit pkgs; };
+                }
+              ];
+              title = "NixOS Modules Search";
+              urlPrefix = "https://github.com/Wavelens/Gradient/blob/main/";
+            };
+          };
+        });
+}
